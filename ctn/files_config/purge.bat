f6c209c8a6811af6b55451117f0aaeb5c4b710f4
Lexplikateur <icaza@veydunet.com>
2024-05-10 07:16:54 +0200

    Delete ctn directory

--- ctn/files_config/purge.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rd /s /q "Ostium.exe.WebView2"
-del /s "sourcepage"
-del /s "sourcepagelst"
-del /s "Archive-DB-FILES-FEED.bat"
-del /s "tempItemAdd.txt"
-del /s "scripts\temp.js.min"
-del /s "tmp.txt"
-exit
