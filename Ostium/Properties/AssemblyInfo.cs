d8355f929944e4e9fdbe4e4d0dfc6a3c5a16bf66
icaza <icaza@veydunet.com>
2025-10-06 17:59:20 +0200

    - Release update

--- Ostium/Properties/AssemblyInfo.cs
+++ Ostium/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.3.27.0")]
-[assembly: AssemblyFileVersion("1.3.27.0")]
+[assembly: AssemblyVersion("1.3.28.0")]
+[assembly: AssemblyFileVersion("1.3.28.0")]
 [assembly: NeutralResourcesLanguage("en")]
