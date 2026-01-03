558277a8f6e3bda52ba159172947a97852c04008
icaza <icaza@veydunet.com>
2026-01-03 18:45:22 +0100

    - Update

--- Ostium/Main_Frm.cs
+++ Ostium/Main_Frm.cs
@@ -178,8 +178,8 @@ namespace Ostium
         /// </summary>
         /// 
         readonly string updtOnlineFile = "https://veydunet.com/2x24/sft/updt/updt_ostium.html"; // <= Change the URL to distribute your version
-        readonly string WebPageUpdate = "https://veydunet.com/ostium/update.html"; // <= Change the URL to distribute your version
-        readonly string versionNow = "31";
+        readonly string WebPageUpdate = "https://veydunet.com/ostium/update.php"; // <= Change the URL to distribute your version
+        readonly string versionNow = "32";
 
         readonly string HomeUrlRSS = "https://veydunet.com/ostium/rss.html";
         int Vrfy = 0;
