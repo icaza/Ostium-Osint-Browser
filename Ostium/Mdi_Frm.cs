2f4cecb761ec9f19d30d5168a48fbbb5219b7375
icaza <icaza@veydunet.com>
2026-02-01 00:11:41 +0100

    - code

--- Ostium/Mdi_Frm.cs
+++ Ostium/Mdi_Frm.cs
@@ -4,8 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Reflection;
-using System.Security.Policy;
 using System.Windows.Forms;
 
 namespace Ostium
