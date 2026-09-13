0872ed7847f3e8e7a07c3af4b457be738cd03928
icaza <icaza@veydunet.com>
2026-09-13 07:47:06 +0200

    Added new script to analyze a URL: - otx.alienvault.com - urlscan.io - crt.sh - www.virustotal.com - platform.censys.io

---
+++ Ostium/scripts/scriptsl/AlienVault.js
@@ -0,0 +1,8 @@
+/**
+ * @file AlienVault.js
+ * @author ICAZA <github.wildly512@passinbox.com>
+ * @version 1.0
+ * @description Security Information and Event Management.
+ */
+
+javascript: window.open(`https://otx.alienvault.com/indicator/domain/${window.location.host}`)
---
+++ Ostium/scripts/scriptsl/CRT.js
@@ -0,0 +1,8 @@
+/**
+ * @file CRT.js
+ * @author ICAZA <github.wildly512@passinbox.com>
+ * @version 1.0
+ * @description Security Information and Event Management.
+ */
+
+javascript: window.open(`https://crt.sh/?q=${window.location.host}`)
